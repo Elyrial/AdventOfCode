923e3367bd1c4d762a939700ca38307d46726fb5
Elyrial <alexpersson03@proton.me>
2025-04-05 22:55:53 +0200

    Combine fetch and run for convenience

--- fetch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cargo run --bin fetch_input -- "$@"
--- run.sh
+++ run.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-cargo run --bin advent_of_code -- run "$@"
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <year> <day>"
+    exit 1
+fi
+
+year=$1
+day=$2
+
+padded_day=$(printf "%02d" "$day")
+
+input_file="inputs/year${year}/day${padded_day}.txt"
+
+# Check if input file exists
+if [ ! -f "$input_file" ]; then
+    echo "Input file not found, fetching..."
+    cargo run --bin fetch_input -- "$year" "$day"
+    
+    if [ $? -ne 0 ]; then
+        echo "Failed to fetch input file"
+        exit 1
+    fi
+fi
+
+cargo run --bin advent_of_code -- run "$year" "$day"
